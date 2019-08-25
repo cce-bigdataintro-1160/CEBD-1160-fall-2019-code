47ce73fb1ba9be7cab22f9a4d99b6cb305d3f7fc
Eduardo Lomonaco <lomonaco.eduardo@gmail.com>
2019-08-25 14:44:17 -0400

    Added class 2 and report for class 11

---
+++ class2-notebook/5-git_commands.sh
@@ -0,0 +1,28 @@
+# git basic commands
+
+# Create a new git repo
+git init
+
+# Clone an existing git repo
+git clone <repository>
+
+# Add a new file to an existing git repo at current directory
+git add <filename>
+
+# Commit recent changes to git repository
+git commit -m "This is my first commit"
+
+# Push existing changes to remote repository
+git push
+
+# Check git remotes
+git remote -v
+
+# Check status of current branch
+git status
+
+# Create new branch
+git branch -b <new_branch>
+
+# Checkout branch
+git checkout <branch_name>
