67a27bf758f1ed6b38b8fa5e9503d38cc8c581fa
Eduardo Lomonaco <lomonaco.eduardo@gmail.com>
2019-08-25 17:32:40 -0400

    Improving class 3

---
+++ class3-notebook/7-scripts.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import sys
+
+argument0 = sys.argv[0]  # Name of script
+argument1 = sys.argv[1]  # Provided argument number 1
+argument2 = sys.argv[2]  # Provided argument number 2
+
+print(argument0)
+print(argument1)
+print(argument2)
---
+++ class3-notebook/9-reading-files.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from os.path import isfile
+
+file_path = 'titanic/test.csv'
+if isfile(file_path):
+    my_file = open(file_path)
+    for index, line in enumerate(my_file.readlines()):
+        stripped_line = line.strip()
+        print(f'The file line {index} has the content "{stripped_line}"')
+else:
+    print('Please provide a valid file!')
