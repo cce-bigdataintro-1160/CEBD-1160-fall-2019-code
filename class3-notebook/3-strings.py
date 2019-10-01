6f5e4e6c13be8b5d20a1c93753369fffdfad488e
eduardolomonaco <eduardo.lomonaco@reactivecore.com>
2019-09-30 20:49:10 -0400

    Updating code content

--- class3-notebook/3-strings.py
+++ class3-notebook/3-strings.py
@@ -15,8 +15,12 @@ print('My name is {0} {1}, nice to meet you!'.format(first_name, last_name))
 
 # String variables have a few special functions, let's see what they can do.
 print(f'The upper/lower functions change your strings case: {first_name.upper()} {last_name.lower()}!')
-print(f'The split function can break your string apart: {first_name.split("h")}!')
 print(f'The len method can show the length of your string {len(first_name)}')
+
+# We can split a phrase into a list of words using the split function on an empty space
+my_phrase = 'This is a long phrase'
+print(f'{my_phrase}')
+print(f'{my_phrase.split(" ")}')
 
 # Strings are actually lists of characters. This means they can "sliced" using the following syntax
 print(f'First letter of first_name {first_name[0]}')
--- class3-notebook/9-reading-files.py
+++ class3-notebook/9-reading-files.py
@@ -1,11 +1,20 @@
-# Let's now see how we can read a file using python
 from os.path import isfile
 
 file_path = 'titanic/test.csv'
+
 if isfile(file_path):
+
+    # To read a file using python we'll use the method `open`
     my_file = open(file_path)
-    for index, line in enumerate(my_file.readlines()):
+
+    for line in my_file.readlines():
+        # Strip will remove any `extra` characters that might be misinterpreted when reading a string
         stripped_line = line.strip()
-        print(f'The file line {index} has the content "{stripped_line}"')
+        print(f'Line has the content "{stripped_line}"')
+
+    # It's important to close files after they've been used to avoid a `resources leak` on the os
+    my_file.close()
+
 else:
     print('Please provide a valid file!')
+
