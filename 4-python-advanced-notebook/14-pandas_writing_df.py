9fab60f49488f71403a3bfad97c80ceae9c53378
Eduardo Lomonaco <lomonaco.eduardo@gmail.com>
2019-10-17 21:00:30 -0400

    Updating code class structure

---
+++ 4-python-advanced-notebook/14-pandas_writing_df.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+
+def pretty_print(name, to_print):
+    print(f'{name}:')
+    print(f'{to_print}\n\n')
+
+
+# Make sure to install xlrd with pip3!!!
+df= pd.read_excel(io='data/titanic/train.xlsx',
+                        sheet_name='Sheet1',
+                        header=0)
+
+df.to_csv('data/titanic/created_train.csv')
+
+
+plt.close()
